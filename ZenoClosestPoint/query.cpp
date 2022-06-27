7dcec0791ce736a82736abc381997cbef81052f1
Jaxon Ko <jrk8@test.hostname.com>
2022-06-27 14:28:25 -0400

    Update query.cpp

--- ZenoClosestPoint/query.cpp
+++ ZenoClosestPoint/query.cpp
@@ -13,31 +13,34 @@ using namespace zeno;
 int main(int argc, char** argv)
 {
 	Vector3<double> a(0.0, 0.0, 0.0);
-	Vector3<double> b(0.0, 5.0, 0.0);
-       	Vector3<double> c(5.0, 5.0, 0.0);
+	Vector3<double> b(7.0, 0.0, 0.0);
+       	Vector3<double> c(0.0, 7.0, 0.0);
+	Vector3<double> d(0.0, 0.0, 7.0);
 
-	Triangle<double> t(a, b, c);	
+	Triangle<double> v1(a, b, c);	
+	Triangle<double> v2(a, b, d);
+	Triangle<double> v3(a, c, d);
+	Triangle<double> v4(b, c, d);
 
 	vector<Triangle<double>> triangles;
-	triangles.push_back(t);
-	
-	t.preprocess();
-
-	cout << t.contains({1.0, 1.0, 0.0}) << endl;
+	triangles.push_back(v1);
+	triangles.push_back(v2);
+	triangles.push_back(v3);
+	triangles.push_back(v4);
 
 	AABBTree<Triangle<double>> scene;
 
 	scene.preprocess(&triangles);
 
 	// closest point query
-	const Triangle<double> **nearestObject;
-	const Vector3<double> queryPoint(6.0, 0.0, 0.0);
+	const Triangle<double> *nearestObject;
+	const Vector3<double> queryPoint(0.0, 0.0, 10.0);
 	double sqDistance;
 
-	scene.findNearestObject(queryPoint, nearestObject, &sqDistance);
+	scene.findNearestObject(queryPoint, &nearestObject, &sqDistance);
 
-	// returns a close-to-zero number?
 	cout << sqDistance << endl;
+	cout << *nearestObject << endl;
 
 	return 0;
 }
